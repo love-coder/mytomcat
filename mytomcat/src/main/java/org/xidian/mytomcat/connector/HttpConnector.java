01c5964a85febc59e709cc9f250c20d74ae25e12
love-coder <xdhanchun@gmail.com>
2016-05-16 16:55:11 +0800

    format

--- mytomcat/src/main/java/org/xidian/mytomcat/connector/HttpConnector.java
+++ mytomcat/src/main/java/org/xidian/mytomcat/connector/HttpConnector.java
@@ -17,9 +17,7 @@ public class HttpConnector implements Runnable {
 
 	// shutdown 命令
 	private static final String SHUTDOWN_COMMAND = "/SHUTDOWN";
-
 	private boolean shutdown = false;
-
 	private Container container;
 
 	public void run() {
@@ -42,17 +40,13 @@ public class HttpConnector implements Runnable {
 				socket = serverSocket.accept();
 				input = socket.getInputStream();
 				output = socket.getOutputStream();
-
 				//创建request，并且对socket的input进行处理。获取uri信息
 				Request request = new Request(input);
 				request.parse();
-
 				//创建response
 				Response response = new Response(output);
 				response.setRequest(request);
-
 				String uri = request.getUri();
-				
 				//判断该uri请求是否为我们配置的servlet，如果是则调用该servlet的service方法
 				if (container!=null && container.findServlet(uri)!=null) {
 					container.invoke(request, response);
@@ -60,9 +54,7 @@ public class HttpConnector implements Runnable {
 					//如果不是servlet，则调用静态资源方法处理请求
 					response.sendStaticResource();
 				}
-				
 				socket.close();
-				
 				//浏览器中输入http://localhost:8080/SHUTDOWN
 				shutdown = request.getUri().equals(SHUTDOWN_COMMAND);
 			} catch (Exception e) {
@@ -81,10 +73,8 @@ public class HttpConnector implements Runnable {
 	
 	public void start(){
 		long now=System.currentTimeMillis();
-		
 		Thread thread=new Thread(this);
 		thread.start();
-		
 		System.out.println("启动连接耗时："+(System.currentTimeMillis()-now)+"ms");
 	}
 }
