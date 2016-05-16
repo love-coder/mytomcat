38442210326b8ed820c436a14e0e607c1db15414
love-coder <xdhanchun@gmail.com>
2016-05-16 16:41:24 +0800

    1.0

---
+++ mytomcat/src/main/java/org/xidian/main/Main.java
@@ -0,0 +1,23 @@
+package org.xidian.main;
+
+import org.xidian.mytomcat.connector.HttpConnector;
+import org.xidian.mytomcat.container.Container;
+import org.xidian.mytomcat.container.SimpleContainer;
+
+/**
+ * 入口
+ * @author HanChun
+ * @version 1.0 2016-5-14
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		//1.启动容器 .
+		Container simpleContainer = new SimpleContainer();
+		simpleContainer.start();
+		//2.在服务端启动监听
+		HttpConnector connector = new HttpConnector();
+		connector.setContainer(simpleContainer);
+		connector.start();
+	}
+}
